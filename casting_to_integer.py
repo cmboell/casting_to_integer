2860f760ea04629210b0cdfd201ef8e2aab34b44
Colby Boell <68253967+cmboell@users.noreply.github.com>
2022-01-12 20:13:34 -0600

    Initial commit

---
+++ casting_to_integer.py
@@ -0,0 +1,22 @@
+"""
+Program: cast_to_integer.py
+Author: Colby Boell
+Last date modified: 01/12/2022
+
+The purpose of this program is to accept any input,
+convert to a integer type and output the integer.
+"""
+# print that prompts user input
+print('Enter a value: ')
+# assigning user input to input by user
+user_input = (input())
+
+print(int(float(user_input)))
+"""
+When the user enters any type of number we get an output of an integer
+When they input a string we get an error
+# Input   Expected Output  Actual Output
+#   12           12              12
+#   43.45        43              43
+#   'test'    ValueError: could not convert string to float: 'test'
+"""
